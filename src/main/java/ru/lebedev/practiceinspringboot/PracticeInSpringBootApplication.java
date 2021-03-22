a6881968882e40ea4f4db1a7aae0042db33a6235
maxim-lebedev <cyue.alle@gmail.com>
2021-03-22 22:14:07 +0300

    init commit

---
+++ src/main/java/ru/lebedev/practiceinspringboot/PracticeInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.lebedev.practiceinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lebedev/practiceinspringboot/PracticeInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lebedev.practiceinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
